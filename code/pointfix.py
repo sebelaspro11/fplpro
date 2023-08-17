dff64bf5dd06864227164b5b6ae0fc0d9129c7dd
sebelaspro11 <132761130+sebelaspro11@users.noreply.github.com>
2023-08-17 23:20:15 +0800

    Update pointfix.py

--- code/pointfix.py
+++ code/pointfix.py
@@ -33,9 +33,15 @@ def perform_point_fixture():
 
     # Access the collections
     collection_details = db["details"]
+    collection_fixture = db["fixture"]
 
     @st.cache_resource
     def fetch_data_history(_collect):
+        # Fetch the data from the collection
+        return pd.DataFrame(list(_collect.find({}, {"_id": 0})))
+    
+    @st.cache_resource
+    def fetch_data_fixture(_collect):
         # Fetch the data from the collection
         return pd.DataFrame(list(_collect.find({}, {"_id": 0})))
 
@@ -44,7 +50,8 @@ def perform_point_fixture():
     st.markdown('##### ***Select Multiple Players For Comparison***')
     # Fetch history and fixtures data
     df_history_2023 = fetch_data_history(collection_details)
-    df_fixtures_2023 = pd.read_csv('data/fixtures_update-20230526.csv')
+    df_fixtures_2023 = fetch_data_fixture(collection_fixture)
+    # df_fixtures_2023 = pd.read_csv('D:/streamlit/fpl/all_fixture_2023-20230815.csv')
     df_fixtures_2023 = df_fixtures_2023.sort_values(by='Gameweek')
 
     # Sidebar filters
@@ -79,12 +86,14 @@ def perform_point_fixture():
             st.altair_chart(c, use_container_width=True, theme="streamlit")
 
             df_fixtures_2023_player = df_fixtures_2023[(df_fixtures_2023['Player Name'] == player) & (df_fixtures_2023['Team'].isin(teams_choice))]
+            df_fixtures_2023_player_next5 = df_fixtures_2023_player.head(5)      
         with tab2:
             
             st.markdown(f'### {player} Next Fixtures')
+            # st.dataframe(df_fixtures_2023.sort_values('Gameweek', ascending=False).reset_index(drop=True))
             color_scale = alt.Scale(domain=[2, 3, 4, 5], range=['green', 'blue', 'yellow', 'red'])
             y_limit = [0, 5]
-            d = alt.Chart(df_fixtures_2023_player).mark_circle(stroke=None, size=200).encode(
+            d = alt.Chart(df_fixtures_2023_player_next5).mark_bar().encode(
                 x=alt.X('Opponent:N', sort=alt.EncodingSortField('Gameweek'), axis=alt.Axis(labelAngle=0)),
                 y=alt.Y('Difficulty:Q', axis=alt.Axis(format='d'), scale=alt.Scale(domain=y_limit)),
                 color=alt.Color('Difficulty:Q', scale=color_scale),
@@ -92,42 +101,5 @@ def perform_point_fixture():
             ).configure_axis(grid=True)
 
             st.altair_chart(d, use_container_width=True, theme="streamlit")
-        
-        # def record_fixture_chart(df_history_2023_player, category, tooltip):
-
-        
-        #     # Filter the data to include only the top 5 players
-        #     df = df_history_2023_player.sort_values(category, ascending=False).reset_index(drop=True).head(5)
-            
-        #     # Create the bar chart
-        #     fig = px.bar(
-        #         df,
-        #         y="Gameweek Points",
-        #         x="Opponent",
-        #         color="Position",
-        #         color_discrete_map=position_colors,  # Set custom colors for each position
-        #         text="Team",
-        #         hover_data=tooltip.get(category, {})
-
-        #     )
-        #     custom_font_family = "Arial"
-            
-        #     # Set the custom font for the text
-        #     fig.update_layout(
-        #         font_family=custom_font_family,
-        #         font_color="black",  # Optionally, set the font color
-        #     )
-            
-        #     return fig
-
-
-
-        # tooltip = {
-        # "Total Points": {"Player Name": True, "Team": True, "Total Points": True}
-        # }
- 
-        # fig_points = record_fixture_chart(df_history_2023_player, "Total Points", tooltip)
-        # st.plotly_chart(fig_points, theme="streamlit", use_container_width=True)
-    
-        #st.altair_chart(d, use_container_width=True, theme="streamlit")
+      
 
