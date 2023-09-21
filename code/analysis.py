58cb1444bcf0b835d25dd6e465188cdfe4983ff1
mannitor <39789038+mannitor@users.noreply.github.com>
2023-09-21 22:52:44 +0800

    up

--- code/analysis.py
+++ code/analysis.py
@@ -684,8 +684,8 @@ def perform_analysis():
         #     red_card_players = df_filtered_player[df_filtered_player['Total RC'] > 0]['Player Name'].tolist()
 
         with tab_rc:
-            red_card_players = df_filtered_player[df_filtered_player['Total RC'] > 0]['Player Name'].tolist()
-            fig_rc = all_chart(df_filtered_player, "Total RC", tooltip, red_card_players)
+            #red_card_players = df_filtered_player[df_filtered_player["Total RC"] > 0]['Player Name'].tolist()
+            fig_rc = all_chart(df_filtered_player, "Total RC", tooltip) #red_card_players)
             st.plotly_chart(fig_rc, theme="streamlit", use_container_width=True)
 
         
