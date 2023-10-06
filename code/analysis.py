50c360b57a4dd95abd2a23b1085ffa26b4741518
sebelaspro11 <132761130+sebelaspro11@users.noreply.github.com>
2023-10-06 18:10:03 +0800

    Update analysis.py

--- code/analysis.py
+++ code/analysis.py
@@ -42,7 +42,7 @@ def perform_analysis():
 
 
     st.markdown('### Player Points and Stats Across 2023/2024 Season')
-    st.markdown('##### ***"Please check 'Show filtered data' after filtering the display.***')
+    st.markdown('##### ***"Please check ""Show filtered data"" after filtering the display.***')
 
 
     # Calculate additional columns
