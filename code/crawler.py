1cbacca1920b50213dbafda72da0c959f39bb9a7
sebelaspro11 <132761130+sebelaspro11@users.noreply.github.com>
2025-01-30 16:54:03 +0800

    Update crawler.py

--- code/crawler.py
+++ code/crawler.py
@@ -1,36 +1,26 @@
-import sys
-import io
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-
 url = "https://www.cinema.com.my/cinemas/cinemas.aspx"
-
 HEADERS = {
-    
     "ACCEPT": "*/*",
     "Accept-Encoding": "gzip, deflate, br, zstd",
     "Accept-Language": "en-GB,en;q=0.5",
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:130.0) Gecko/20100101 Firefox/130.0"
-    
 }
-    
 
-
-response = requests.get(url)
-
+response = requests.get(url, headers=HEADERS)
 soup = BeautifulSoup(response.text, "html.parser")
-
 regions = soup.find_all("div", class_="SecHeader")
 
-# Extract the text within each a tag in the regions
+# Extract text from <a> tags
 text_regions = [a_tag.get_text() for region in regions for a_tag in region.find_all("a")]
 
-# Remove 'Announcement' from the list
-#filtered_values = [value for value in text_regions if value != 'Announcement']
+# Convert list to DataFrame
+df = pd.DataFrame(text_regions, columns=["Region"])
 
-print(text_regions)
+# Save to CSV
+df.to_csv("test.csv", index=False)
 
-text_regions.to_csv("test.csv")
-text_regions.to_csv("data/test.csv")
+print("CSV file has been successfully created!")
